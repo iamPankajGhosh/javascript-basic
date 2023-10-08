2a19c0c71073ffa5eb7272055ff477baddcbaa02
Pankaj Ghosh <140588883+iamPankajGhosh@users.noreply.github.com>
2023-10-08 03:53:00 +0000

    javscript variables done

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,11 @@
+const accountId = 1414;
+let accountEmail = "demo@gmail.com";
+var accountPassword = "5454";
+accountCity = "kolkata";
+let accountState;
+
+accountEmail = "demo.google@gamil.com";
+accountPassword = "1234";
+accountCity = "bangaluru";
+
+console.table([accountId, accountEmail, accountPassword, accountCity, accountState]);
