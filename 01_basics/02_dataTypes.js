3a31385f44db2e90912de2ecf847389bff82ae14
Pankaj Ghosh <140588883+iamPankajGhosh@users.noreply.github.com>
2023-10-09 06:52:01 +0000

    datatypes done

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,18 @@
+let name = "pankaj";
+let age = 18;
+let isLoggedIn = true;
+
+/**
+ * datatypes
+ */
+
+// number
+// bigint
+// string
+// boolean
+// null
+// undefine
+// symbol
+// object
+
+console.log(typeof name);
