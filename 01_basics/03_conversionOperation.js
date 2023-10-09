af8622ecaaa91511b7d24e67eb2c325ec25129f1
Pankaj Ghosh <140588883+iamPankajGhosh@users.noreply.github.com>
2023-10-09 07:23:29 +0000

    Conversion operation done

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,19 @@
+let score = undefined;
+
+console.log(score);
+console.log(typeof score);
+
+let valueInNumber = Number(score);
+
+console.log(valueInNumber);
+console.log(typeof valueInNumber);
+
+/**
+ * conversion in number
+ */
+
+// "33" => 33
+// "33abc" => NaN (Not a Number)
+// true => 1 / false => 0
+// null => 0
+// undefined => NaN
