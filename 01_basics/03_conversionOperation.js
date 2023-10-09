c72dc0c9b7af44611d9ac269ecc13ed140a01ed0
Pankaj Ghosh <140588883+iamPankajGhosh@users.noreply.github.com>
2023-10-09 08:02:11 +0000

    Conversion operation 2 done

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -17,3 +17,32 @@ console.log(typeof valueInNumber);
 // true => 1 / false => 0
 // null => 0
 // undefined => NaN
+
+/**
+ * string operation
+ */
+
+let str1 = "hello";
+let str2 = " pankaj";
+
+let str3 = str1 + str2;
+
+console.log(str3);
+
+/**
+ * postfix & prefix increament
+ */
+
+// postfix increament
+let x = 3;
+const y = x++;
+
+console.log(x);
+console.log(y);
+
+// prefix increament
+let a = 3;
+const b = ++a;
+
+console.log(a);
+console.log(b);
