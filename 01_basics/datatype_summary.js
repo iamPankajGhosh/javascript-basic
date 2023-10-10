8f453728840b4ff2463d2264442e0f181c36fa1f
Pankaj Ghosh <140588883+iamPankajGhosh@users.noreply.github.com>
2023-10-10 18:45:56 +0000

    datatype summary done

---
+++ 01_basics/datatype_summary.js
@@ -0,0 +1,47 @@
+/**
+ * datatypes
+ */
+
+// primitive datatypes
+// non-primitive / reference datatypes
+
+/**
+ * primitive datatype
+ */
+
+// string
+const name = "pankaj";
+
+// number
+const score = 100;
+
+// boolean
+const isLoggedIn = false;
+
+// null
+const outsideTemp = null;
+
+// undefined
+let userEmail;
+
+// symbol
+const id = Symbol("123");
+const anotherId = Symbol("123");
+
+/**
+ * reference datatype
+ */
+
+// array
+const heros = ["saktiman","naagraj", "doga"];
+
+// objects
+const myObject = {
+    name: "pankaj",
+    age: 18
+}
+
+//function
+const myFunction = function() {
+    console.log("hello world");
+}
