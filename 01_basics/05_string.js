ac42a760657a36c5796548cf812d431e9477f074
Pankaj Ghosh <140588883+iamPankajGhosh@users.noreply.github.com>
2023-10-12 18:43:51 +0000

    string interpolation done

---
+++ 01_basics/05_string.js
@@ -0,0 +1,11 @@
+/**
+ * string datatype
+ */
+const name = "pankaj ghosh";
+const repoCount = 50;
+
+// old way
+console.log(name + repoCount + "value");
+
+// mordern way
+console.log(`hello! my name is ${name} and my repo count is ${repoCount}`);
