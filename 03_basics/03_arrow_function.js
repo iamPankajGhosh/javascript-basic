1ca109e88f2e1f8b7e67e9d4815e40d22c50d4d7
Pankaj Ghosh <140588883+iamPankajGhosh@users.noreply.github.com>
2023-10-21 17:55:43 +0000

    explicit & implicit return done

--- 03_basics/03_arrow_function.js
+++ 03_basics/03_arrow_function.js
@@ -7,3 +7,15 @@ const myFunction = () => {
 }
 
 myFunction();
+
+// explicit return
+const addTwoNumber = (num1, num2) => {
+    return num1 + num2;// include return keyword
+}
+
+console.log(addTwoNumber(2,3));
+
+// implicit return
+const addTwo = (num1, num2) => (num1 + num2);
+
+console.log(addTwo(3,3));
