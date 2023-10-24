a9344dfeff2a3b5d66ef35f690e245b1e7a7b597
Pankaj Ghosh <140588883+iamPankajGhosh@users.noreply.github.com>
2023-10-24 17:46:10 +0000

    terniary operator done

--- 04_control_flow/truthly.js
+++ 04_control_flow/truthly.js
@@ -34,3 +34,7 @@ let val1;
 val1 = null ?? 10 ?? 15;
 
 console.log(val1);
+
+//terniary operator
+const iceTeaPrice = 100;
+iceTeaPrice <= 80 ? console.log("less than 80") : console.log("more than 80");
