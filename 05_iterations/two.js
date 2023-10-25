c263331c6308a0ea566afabc96d52065757394b7
Pankaj Ghosh <140588883+iamPankajGhosh@users.noreply.github.com>
2023-10-25 07:07:12 +0000

    while loop done

---
+++ 05_iterations/two.js
@@ -0,0 +1,17 @@
+/**
+ * while loop
+ */
+
+let index = 0;
+while (index <= 10) {
+    console.log(`index value is ${index}`);
+    index = index + 2;
+}
+
+const myArr = ["flash", "batman", "superman"];
+let arr = 0;
+
+while(arr < myArr.length){
+    console.log(myArr[arr]);
+    arr++;
+}
