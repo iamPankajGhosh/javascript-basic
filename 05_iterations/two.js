707ca47b2ca9cb090e384a952a3ce593f8785d68
Pankaj Ghosh <140588883+iamPankajGhosh@users.noreply.github.com>
2023-10-25 11:17:55 +0000

    do while loop done

--- 05_iterations/two.js
+++ 05_iterations/two.js
@@ -15,3 +15,13 @@ while(arr < myArr.length){
     console.log(myArr[arr]);
     arr++;
 }
+
+/**
+ * do while loop
+ */
+
+let score = 1;
+do {
+    console.log(`Score is : ${score}`);
+    score++;
+} while (score <= 10);
