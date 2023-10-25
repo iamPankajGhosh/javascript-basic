e4718e8a94639a3410d97b7c9fbf04f2c4755d61
Pankaj Ghosh <140588883+iamPankajGhosh@users.noreply.github.com>
2023-10-25 06:55:25 +0000

    basic loop done

--- 05_iterations/one.js
+++ 05_iterations/one.js
@@ -6,3 +6,37 @@ for (let i = 0; i <= 10; i++) {
     const element = i;
     console.log(element);
 }
+
+for (let index = 1; index <= 5; index++) {
+    console.log(`table of ${index}`);
+    for (let j = 1; j <= 10; j++) {
+        console.log(index + " * " + j + " = " + index*j);  
+        
+    }
+}
+
+// loop on array
+const myArray = ["flash", "batman", "superman"];
+
+for (let index = 0; index < myArray.length; index++) {
+    const element = myArray[index];
+    console.log(element);
+}
+
+// breake and continue
+
+for (let index = 1; index <= 20; index++) {
+    if(index == 5){
+        console.log("detected 5");
+        break;
+    }
+    console.log(index);
+}
+
+for (let index = 1; index <= 20; index++) {
+    if(index == 5){
+        console.log("detected 5");
+        continue;
+    }
+    console.log(index);
+}
