fe83f83a6846c154acae2e29af266c60d3abaf9b
Pankaj Ghosh <140588883+iamPankajGhosh@users.noreply.github.com>
2023-10-26 18:16:15 +0000

    basic for-of loop done

---
+++ 05_iterations/three.js
@@ -0,0 +1,16 @@
+/**
+ * high order loop
+ */
+
+// for of loop
+const arr = [1, 2, 3, 4, 5];
+
+for (const num of arr) {
+    console.log(num);
+}
+
+const greeting = "hello world";
+
+for (const greet of greeting) {
+    console.log(greet);
+}
