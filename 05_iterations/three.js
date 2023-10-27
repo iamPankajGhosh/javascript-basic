a0c426a5a13e609358c6b11a9d472b55fa3b9267
Pankaj Ghosh <140588883+iamPankajGhosh@users.noreply.github.com>
2023-10-27 18:04:25 +0000

    loop on map done

--- 05_iterations/three.js
+++ 05_iterations/three.js
@@ -14,3 +14,15 @@ const greeting = "hello world";
 for (const greet of greeting) {
     console.log(greet);
 }
+
+// maps
+const map = new Map();
+
+map.set("IN", "India");
+map.set("USA", "United States of America");
+map.set("FR", "France");
+map.set("IN", "India");
+
+for (const [key, value] of map) {
+    console.log(`${key} -> ${value}`);
+}
