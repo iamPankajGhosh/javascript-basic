3800776b0bd00bbfaf2f18523c75a745c301346b
Pankaj Ghosh <140588883+iamPankajGhosh@users.noreply.github.com>
2023-11-13 18:27:48 +0000

    another way of promise is done

--- 08_advance_one/promises.js
+++ 08_advance_one/promises.js
@@ -1,42 +1,69 @@
-const promiseOne = new Promise(function(resolve, reject){
-    //DB calls
-    setTimeout(function(){
-        console.log("Async task is completed");
-        resolve();
-    }, 1000)
-});
+// const promiseOne = new Promise(function(resolve, reject){
+//     //DB calls
+//     setTimeout(function(){
+//         console.log("Async task is completed");
+//         resolve();
+//     }, 1000)
+// });
 
-promiseOne.then(function(){
-    console.log("Promise consumed");
-})
+// promiseOne.then(function(){
+//     console.log("Promise consumed");
+// })
+
+// /**
+//  * another way
+//  */
+
+// new Promise(function(resolve, reject){
+//     setTimeout(function(){
+//         console.log("Async task two");
+//         resolve();
+//     }, 1000);
+// }).then(function(){
+//     console.log("Async 2 resolved");
+// })
+
+// /**
+//  * another way
+//  */
+
+// const promiseThree = new Promise(function(resolve, reject){
+//     setTimeout(function(){
+//         console.log("Async three done");
+//         resolve({
+//             username : "pankaj",
+//             email : "pankaj@gmail.com"
+//         });
+//     }, 1000);
+// });
+
+// promiseThree.then(function(user){
+//     console.log(user);
+// });
 
 /**
  * another way
  */
 
-new Promise(function(resolve, reject){
+const promiseFour = new Promise(function(resolve, reject){
     setTimeout(function(){
-        console.log("Async task two");
-        resolve();
+        let error = true;
+        if(!error){
+            resolve({
+                username : "pankaj",
+                password : "1234"
+            })
+        } else {
+            reject("ERROR : Something went wrong");
+        }
     }, 1000);
-}).then(function(){
-    console.log("Async 2 resolved");
 })
 
-/**
- * another way
- */
-
-const promiseThree = new Promise(function(resolve, reject){
-    setTimeout(function(){
-        console.log("Async three done");
-        resolve({
-            username : "pankaj",
-            email : "pankaj@gmail.com"
-        });
-    }, 1000);
-});
-
-promiseThree.then(function(user){
+promiseFour.then((user) => {
     console.log(user);
-});
+    return user.username;
+}).then((username) => {
+    console.log(username);
+}).catch((error) => {
+    console.log(error);
+})
