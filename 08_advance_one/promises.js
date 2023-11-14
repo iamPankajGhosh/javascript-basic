1c7544b5152a36e5e1b92a2a7d96d6af6e9b6781
Pankaj Ghosh <140588883+iamPankajGhosh@users.noreply.github.com>
2023-11-14 05:13:15 +0000

    try-catch block done

--- 08_advance_one/promises.js
+++ 08_advance_one/promises.js
@@ -98,3 +98,19 @@ async function consumePromiseFive(){
 }
 
 consumePromiseFive();
+
+/**
+ * implementation
+ */
+
+async function getAllUsers() {
+    try {
+        const response = await fetch("https://jsonplaceholder.typicode.com/users")
+        const data = await response.json();
+        console.log(data);
+    } catch (error) {
+       console.log("Error : ", error); 
+    }
+}
+
+getAllUsers();
