104ba2f98f1e5f8d68654024e516018f280799af
Pankaj Ghosh <140588883+iamPankajGhosh@users.noreply.github.com>
2023-11-17 08:42:35 +0000

    opp done

--- 09_classes_and_oop/oop.js
+++ 09_classes_and_oop/oop.js
@@ -14,3 +14,30 @@ const user = {
 
 console.log(user.username);
 console.log(user.getUserDetails());
+
+/**
+ * constructor function
+ */
+
+function User(username, logInCount, signIn) {
+    this.username = username;
+    this.loglogInCount =logInCount;
+    this.signIn = signIn;
+    this.greeting = function(){
+        console.log(`Welcome ${this.username}`);
+    };
+
+    return this;
+}
+
+const userOne = new User("raj", 12, true);
+
+// step 1 : create new empty object for THIS keyword
+// step 2 : call constructor function for NEW keyword and packed all those argument
+// step 3 : inject all those packed argument inside THIS
+// step 4 : finally get this object
+
+const userTwo = new User("jim", 11, false);
+
+console.log(userOne);
+console.log(userTwo);
