e9d6fc551c02c01c2001c8cb49e654fcc023396c
Pankaj Ghosh <pankajghosh.github@gmail.com>
2024-07-15 00:13:41 +0530

    task 4 done

--- 30dayschallenge/day_1.js
+++ 30dayschallenge/day_1.js
@@ -9,3 +9,20 @@ console.log(name);
 // task 3
 const isLoggedIn = true;
 console.log(isLoggedIn);
+
+// task 4
+let age = 18;
+let username = "one";
+let isValidate = true;
+let userData = {
+  fullName: "one two",
+  firstName: "one",
+  lastName: "two",
+};
+let updateDate = ["firstName", "lastName"];
+
+console.log(typeof age); // number
+console.log(typeof username); // string
+console.log(typeof isValidate); // boolean
+console.log(typeof userData); // object
+console.log(typeof updateDate); // object
