9ecbe619696fab2066a73a79dfe035414ce793ae
Pankaj Ghosh <pankajghosh.github@gmail.com>
2024-07-16 00:29:19 +0530

    day 2 complete

---
+++ 30dayschallenge/day_2.js
@@ -0,0 +1,79 @@
+// task 1
+let resultOne = 10 + 5;
+console.log(resultOne);
+
+// task 2
+let resultTwo = 10 - 5;
+console.log(resultTwo);
+
+// task 3
+let resultThree = 10 * 5;
+console.log(resultThree);
+
+// task 4
+let resultFour = 10 / 5;
+console.log(resultFour);
+
+// task 5
+let resultFive = 10 % 3;
+console.log(resultFive);
+
+// task 6
+let resultSix = 0;
+resultSix += 5;
+console.log(resultSix);
+
+// task 7
+let resultSeven = 10;
+resultSeven -= 5;
+console.log(resultSeven);
+
+// task 8
+let compareOne = 10 < 5;
+console.log(compareOne);
+
+// task 9
+let compareTwo = 10 > 5;
+console.log(compareTwo);
+
+// task 10
+let compareThree = 10 >= 5;
+console.log(compareThree);
+
+// task 11
+let compareFour = 10 <= 5;
+console.log(compareFour);
+
+// task 12
+let compareFive = 10 == 5;
+console.log(compareFive);
+
+// task 13
+let compareSix = 10 === 5;
+console.log(compareSix);
+
+// task 14
+if (5 > 0 && 5 < 10) {
+  console.log("Number between 0 to 10");
+} else {
+  console.log("Number not between 0 to 10");
+}
+
+// task 15
+if (5 > 0 || 5 < 10) {
+  console.log("Number between 0 to 10");
+} else {
+  console.log("Number not between 0 to 10");
+}
+
+// task 16
+const isLoggedIn = true;
+if (!isLoggedIn) {
+  console.log("Log in first");
+} else {
+  console.log("Welcome");
+}
+
+// task 17
+const number = -5;
+console.log(number > 0 ? "Positive" : "Negetive");
